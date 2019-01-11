522b9c0a13b285aabb4c786fd53ef073a58eb942
LUNA <im6705@naver.com>
2019-01-11 16:27:19 +0900

    Update Belike.cs

--- CommunityBot/Modules/Fun/Belike.cs
+++ CommunityBot/Modules/Fun/Belike.cs
@@ -1,1 +1,13 @@
+using System.Threading.Tasks;
+using CommunityBot.Extensions;
+using CommunityBot.Features.Trivia;
+using Discord.Commands;
 
+namespace CommunityBot.Modules.Fun
+{
+    public class Belike : ModuleBase<MiunieCommandContext>
+    {
+                    ///
+                    /// need_add
+    }
+}
